6d742afba34deac8c05f0e096c287b177f0ddd67
Murat Karaca <murat__karaca@windowslive.com>
2025-12-15 23:58:49 +0300

    -updated the cron settings.

--- backend/src/scheduler/price-alert.scheduler.ts
+++ backend/src/scheduler/price-alert.scheduler.ts
@@ -11,7 +11,7 @@ export class PriceAlertScheduler {
         @InjectQueue('price-alert-queue') private priceAlertQueue: Queue,
     ) { }
 
-    @Cron(CronExpression.EVERY_MINUTE)
+    @Cron(CronExpression.EVERY_30_MINUTES)
     async handleCron() {
         this.logger.log('CRON JOB triggered: Checking price-alert-trigger job to queue.');
 
